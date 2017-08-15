c594401220e402149d5c9745eb523746d68b2104
Chris Brown <chrisbrownie@users.noreply.github.com>
2017-08-15 21:36:17 +1000

    oops

--- client/init.sh
+++ client/init.sh
@@ -3,6 +3,6 @@ exec_path=/home/pi/cartracker/
 
 # Ensure we've got the latest version of the code
 echo 'getting latest copy of repository'
-cd exec_path
-git checkout master
+cd $exec_path
+git pull
 echo 'got latest copy of repository'
