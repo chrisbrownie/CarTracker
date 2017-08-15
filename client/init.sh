c605a18a18db122ca948bf1ab05b83289d2f1dca
Chris Brown <chrisbrownie@users.noreply.github.com>
2017-08-15 21:44:47 +1000

    qf

--- client/init.sh
+++ client/init.sh
@@ -2,7 +2,8 @@
 exec_path='/home/pi/cartracker/'
 
 # Ensure we've got the latest version of the code
-echo 'getting latest copy of repository'
+
 cd $exec_path
+
 git pull
-echo 'got latest copy of repository'
+
