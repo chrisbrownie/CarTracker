2e5caa3d0aa0d2a54376a3c5ba28d1eb2c50eb44
Chris Brown <chrisbrownie@users.noreply.github.com>
2017-08-15 21:32:50 +1000

    updates

--- client/init.sh
+++ client/init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+exec_path=/home/pi/cartracker/
 
 # Ensure we've got the latest version of the code
+echo 'getting latest copy of repository'
+cd exec_path
 git checkout master
-
+echo 'got latest copy of repository'
